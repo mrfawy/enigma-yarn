1ebcf7c3780b1cbefb720427643ae2091d2e881e
mrfawy <mrfawy@yahoo.com>
2016-03-02 14:55:33 -0700

    fix

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-mvn clean install
-ssh mabdelra@silver-edge.db.aexp.com rm -f /axp/gcp/cpsetlh/dev/enigma-yarn-app-1.1.0.jar 
-scp /cygdrive/c/Users/mabdelra/.m2/repository/com/home/enigma-yarn-app/1.1.0/enigma-yarn-app-1.1.0.jar mabdelra@silver-edge.db.aexp.com:/axp/gcp/cpsetlh/dev
